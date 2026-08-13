12a966b19e7d67f8bb5feb8a379cc674a86f5713
swift <samuelkw1999@gmail.com>
2026-08-12 23:38:46 -0400

    change resolution of live video

--- read.py
+++ read.py
@@ -7,18 +7,26 @@ def rescaleFrame(frame, scale = 0.75):
     dimensions = (width, height)
     return cv.resize(frame, dimensions, interpolation = cv.INTER_AREA)
 
-img_read = cv.imread('kirby.jpg')
-cv.imshow('kirby', img_read)
-resized_frame = rescaleFrame(img_read)
-cv.imshow('kirby resized', resized_frame)
-cv.waitKey(0)
+#change resolution of live video specifically
+def changeRes(capture, width, height):
+    capture.set(3, width)
+    capture.set(4, height)
+    return capture
 
+# img_read = cv.imread('kirby.jpg')
+# cv.imshow('kirby', img_read)
+# resized_frame = rescaleFrame(img_read)
+# cv.imshow('kirby resized', resized_frame)
+# cv.waitKey(0)
 
-# video = cv.VideoCapture(0)
-# while (True):
-#     isTrue, frame = video.read()
-#     cv.imshow('video', frame)
-#     if(cv.waitKey(20) & 0xFF==ord('d')):
-#         break
-# video.release()
-# cv.destroyAllWindows()
+capture = cv.VideoCapture(0)
+reschangedCapture = changeRes(capture,1920,1080)
+while (True):
+    #isTrue, frame = capture.read()
+    isTrue, reschanged_frame = reschangedCapture.read()
+    #cv.imshow('video', frame)
+    cv.imshow('resolution changed', reschanged_frame)
+    if(cv.waitKey(20) & 0xFF==ord('d')):
+        break
+reschangedCapture.release()
+cv.destroyAllWindows()
