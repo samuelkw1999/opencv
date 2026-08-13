fa0ffeefb95557bfd80c14da2da61e9eabb4b37b
swift <samuelkw1999@gmail.com>
2026-08-12 23:22:39 -0400

    resize frame method 1

--- read.py
+++ read.py
@@ -1,18 +1,24 @@
 import cv2 as cv
 
-""" img_read = cv.imread('images/kirby.jpg')
+#rescale image by 75%
+def rescaleFrame(frame, scale = 0.75):
+    width = int(frame.shape[1] * scale) #shape[1] is width of frame, multiply by 0.75 and cast to integer
+    height = int(frame.shape[0] * scale) #shape[0] is height of frame, multiply by 0.75 and cast to integer
+    dimensions = (width, height)
+    return cv.resize(frame, dimensions, interpolation = cv.INTER_AREA)
 
-cv.imshow('kirby', img_read) """
+img_read = cv.imread('kirby.jpg')
+cv.imshow('kirby', img_read)
+resized_frame = rescaleFrame(img_read)
+cv.imshow('kirby resized', resized_frame)
+cv.waitKey(0)
 
-video = cv.VideoCapture(0)
 
-while (True):
-    isTrue, frame = video.read()
-    cv.imshow('video', frame)
-    if(cv.waitKey(20) & 0xFF==ord('d')):
-        break
-
-video.release()
-cv.destroyAllWindows()
-
-#cv.waitKey(0)
+# video = cv.VideoCapture(0)
+# while (True):
+#     isTrue, frame = video.read()
+#     cv.imshow('video', frame)
+#     if(cv.waitKey(20) & 0xFF==ord('d')):
+#         break
+# video.release()
+# cv.destroyAllWindows()
